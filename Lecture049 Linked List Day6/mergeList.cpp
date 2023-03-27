857f50e75f9fc82d3c92e932c88eb73f0c664e4a
prabhat-4165 <116863529+prabhat-4165@users.noreply.github.com>
2023-03-27 16:48:39 +0530

    update the code for merge two sorted lists.

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -21,51 +21,50 @@
     };
 
 ************************************************************/
-
-void solve(Node<int>* first, Node<int>* second) {
+Node *solve(Node<int> *first, Node<int> *second){
+      // if only one node is present in the linked list
+      if (first -> next == NULL){
+          first -> next = second;
+          return first;
+      }
     
+    Node<int> *curr1 = first;
+    Node<int> *next1 = first -> next;
+    Node<int> *curr2 = second;
+    Node<int> *next2 = second -> next;
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
+    while (next1 != NULL && curr2 != NULL){
+        if ((curr2 -> data >= curr1 -> data) && (curr2 -> data <= next1 -> data)){
             curr1 -> next = curr2;
+            next2 = curr2 -> next;
             curr2 -> next = next1;
             curr1 = curr2;
             curr2 = next2;
         }
-        else {
-            
-        }
-        
-        
+           else {
+               curr1 = curr1 -> next;
+               next1 = next1 -> next;
+               
+               if (next1 == NULL){
+                  curr1 -> next = curr2;
+                  return first;
+               }
+           }
     }
-    
-    
+       return first;
 }
 
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
-{
-    if(first == NULL)
-        return second;
-    
-    if(second == NULL)
-        return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
-    }
-    
-    
-}
+Node<int>* sortedMerge(Node<int>* head1, Node<int>* head2)  
+{  
+   if (head1 == NULL)
+       return head2;
+       
+       if (head2 == NULL)
+          return head1;
+          
+          if (head1 -> data <= head2 -> data)
+             return solve(head1,head2);
+              
+              else 
+             return solve(head2,head1);
+}  
